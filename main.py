52d5ad5c0551643fda6d65f154dbed48312ba374
Sophie Chávez <sophiexp@live.de>
2025-11-15 19:34:36 +0100

    main angepasst

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("Unsere Polynominterpolationen")
+#hier kommen Imports hin
+# Eingabe, Werte prüfen, Eingabe für weitere Punkte und Ausgabe (app)  
+print("Polynominterpolationen, hier werden die Stützstellen abgefragt.")
